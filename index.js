b8e8303f28aa1c8711fa75bc90f4a74db0d9acfb
sousou1216 <sousou1216@yahoo.co.jp>
2024-04-16 08:11:36 +0900

    リファクタ

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-function loadData(value) {
+async function loadData(value) {
   document.getElementById('loading').style.display = 'block';
 
-  if (value === 'allchara') { allchara(); }
-  if (value === 'onisatsu') { onisatsu(); }
-  if (value === 'hashira') { hashira(); }
-  if (value === 'oni') { oni(); }
+  await commonView(value);
 
-  id_li = ['js-allchara', 'js-onisatsu', 'js-hashira', 'js-oni']
-  for (i of id_li) {
-    document.getElementById(i).style.display = 'none';
-  }
-
-  document.getElementById(`js-${value}`).style.display = 'flex';
+  // document.getElementById('js-target').style.display = 'flex';
   document.getElementById('loading').style.display = 'none';
 }
 
-function commonView(json, element_target) {
+async function commonView(value) {
+  const response = await fetch(`https://ihatov08.github.io/kimetsu_api/api/${value}.json`);
+  const json = await response.json();
+  const element_target = document.getElementById('js-target');
+
   element_target.innerHTML = '';
   
   for (const i of json) {
@@ -32,32 +28,4 @@ function commonView(json, element_target) {
   }
 }
 
-async function allchara() {
-  const response = await fetch('https://ihatov08.github.io/kimetsu_api/api/all.json');
-  const json = await response.json();
-  const element_target = document.getElementById('js-allchara');
-  commonView(json, element_target);
-}
-
-async function onisatsu() {
-  const response = await fetch('https://ihatov08.github.io/kimetsu_api/api/kisatsutai.json');
-  const json = await response.json();
-  const element_target = document.getElementById('js-onisatsu');
-  commonView(json, element_target);
-}
-
-async function hashira() {
-  const response = await fetch('https://ihatov08.github.io/kimetsu_api/api/hashira.json');
-  const json = await response.json();
-  const element_target = document.getElementById('js-hashira');
-  commonView(json, element_target);
-}
-
-async function oni() {
-  const response = await fetch('https://ihatov08.github.io/kimetsu_api/api/oni.json');
-  const json = await response.json();
-  const element_target = document.getElementById('js-oni');
-  commonView(json, element_target);
-}
-
-allchara();
+commonView('all');
